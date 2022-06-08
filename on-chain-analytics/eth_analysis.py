50a8adc1d073b2cc72292e07e5e2a61900b82994
anfro.eth <aridding18@gmail.com>
2022-06-08 17:46:22 +0000

    etherescan connection

--- on-chain-analytics/eth_analysis.py
+++ on-chain-analytics/eth_analysis.py
@@ -1,2 +1,8 @@
 from etherscan import Etherscan
-eth = Etherscan(YOUR_API_KEY) # key in quotation marks
+import os 
+
+ETHERSCAN_KEY = os.getenv("VAR")
+
+eth = Etherscan(ETHERSCAN_KEY) # key in quotation marks
+
+latest_price = eth.get_eth_last_price()
